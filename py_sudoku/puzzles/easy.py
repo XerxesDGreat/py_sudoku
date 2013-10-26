afe47f3a59fe75962562b53a6f11533d1dc8ce07
Josh Wickham <josh@i-josh.com>
2013-10-26 14:53:06 -0700

    change format of puzzle

--- py_sudoku/puzzles/easy.py
+++ py_sudoku/puzzles/easy.py
@@ -2,5 +2,5 @@
 __author__ = 'josh'
 
 puzzles = [
-    [1,0,0,0,9,3,0,5,2,9,0,0,0,6,0,1,3,0,0,4,3,5,0,0,0,9,8,3,0,0,0,7,0,0,8,0,0,0,0,8,0,4,0,0,0,0,8,0,0,5,0,0,0,7,6,2,0,0,0,9,8,7,0,0,1,7,0,8,0,0,0,6,8,3,0,6,2,0,0,0,5]
+    [1,None,None,None,9,3,None,5,2,9,None,None,None,6,None,1,3,None,None,4,3,5,None,None,None,9,8,3,None,None,None,7,None,None,8,None,None,None,None,8,None,4,None,None,None,None,8,None,None,5,None,None,None,7,6,2,None,None,None,9,8,7,None,None,1,7,None,8,None,None,None,6,8,3,None,6,2,None,None,None,5]
 ]
